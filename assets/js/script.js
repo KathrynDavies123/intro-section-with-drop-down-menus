fc8c3bc508516f7e2746162a74f6b9ef381d0f96
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2022-12-07 18:57:06 +0100

    javascript done, started mobile layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 let menubutton = document.querySelector(".menubutton");
+let bodybackground = document.querySelector(".backgroundcolor");
 let mainnavcontainer = document.querySelector(".mainnavcontainer");
 let dropdowns = document.querySelectorAll(".dropdown");
 
 menubutton.addEventListener("click", function () {
-  if (mainnavcontainer.style.display === "block") {
-    mainnavcontainer.style.display = "none";
-  } else {
-    mainnavcontainer.style.display = "block";
-  }
+    mainnavcontainer.classList.toggle("open");
+    bodybackground.classList.toggle("on");
+    if (mainnavcontainer.classList.contains("open")) {
+        menubutton.innerHTML =  '<img src="./assets/images/icon-close-menu.svg" alt="close menu button" />';
+    }
+    else {
+        menubutton.innerHTML = '<img src="./assets/images/icon-menu.svg" alt="menu button" />';
+    }
 });
 
 dropdowns.forEach((item) => {
@@ -15,10 +19,10 @@ dropdowns.forEach((item) => {
     event.preventDefault();
     let expansion = item.nextElementSibling;
     item.classList.toggle("expanded");
-    if (expansion.style.display === "block") {
+    if (expansion.style.display === "flex") {
       expansion.style.display = "none";
     } else {
-      expansion.style.display = "block";
+      expansion.style.display = "flex";
     }
   });
 });
