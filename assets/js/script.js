d375a7aab51f839b49b0790179f12f3d1341e81d
KathrynDavies123 <kathryn.davies1233@gmail.com>
2022-12-07 15:43:59 +0100

    started setting up nav menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
+let menubutton = document.querySelector(".menubutton");
+let mainnavcontainer = document.querySelector(".mainnavcontainer");
 let dropdowns = document.querySelectorAll(".dropdown");
 
+menubutton.addEventListener("click", function () {
+  if (mainnavcontainer.style.display === "block") {
+    mainnavcontainer.style.display = "none";
+  } else {
+    mainnavcontainer.style.display = "block";
+  }
+});
+
 dropdowns.forEach((item) => {
-    item.addEventListener("click", function (event) {
-        event.preventDefault();
-        let expansion = item.nextElementSibling;
-        if (expansion.style.display === "block") {
-            expansion.style.display ="none";
-        }
-        else {
-            expansion.style.display = "block";
-        }
-    })
-})
+  item.addEventListener("click", function (event) {
+    event.preventDefault();
+    let expansion = item.nextElementSibling;
+    item.classList.toggle("expanded");
+    if (expansion.style.display === "block") {
+      expansion.style.display = "none";
+    } else {
+      expansion.style.display = "block";
+    }
+  });
+});
