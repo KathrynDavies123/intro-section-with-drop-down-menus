5030a343f27be71b3c375ce0e7917d25121f4174
KathrynDavies123 <kathryn.davies1233@gmail.com>
2022-12-10 20:43:16 +0100

    added that menus close when another is opened

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,34 @@ let menubutton = document.querySelector(".menubutton");
 let bodybackground = document.querySelector(".backgroundcolor");
 let mainnavcontainer = document.querySelector(".mainnavcontainer");
 let dropdowns = document.querySelectorAll(".dropdown");
+let expanded = document.querySelectorAll(".expanded");
 
 menubutton.addEventListener("click", function () {
-    mainnavcontainer.classList.toggle("open");
-    bodybackground.classList.toggle("on");
-    if (mainnavcontainer.classList.contains("open")) {
-        menubutton.innerHTML =  '<img src="./assets/images/icon-close-menu.svg" alt="close menu button" />';
-    }
-    else {
-        menubutton.innerHTML = '<img src="./assets/images/icon-menu.svg" alt="menu button" />';
-    }
+  mainnavcontainer.classList.toggle("open");
+  bodybackground.classList.toggle("on");
+  if (mainnavcontainer.classList.contains("open")) {
+    menubutton.innerHTML =
+      '<img src="./assets/images/icon-close-menu.svg" alt="close menu button" />';
+  } else {
+    menubutton.innerHTML =
+      '<img src="./assets/images/icon-menu.svg" alt="menu button" />';
+  }
 });
 
-dropdowns.forEach((item) => {
-  item.addEventListener("click", function (event) {
-    event.preventDefault();
-    let expansion = item.nextElementSibling;
-    item.classList.toggle("expanded");
-    if (expansion.style.display === "flex") {
-      expansion.style.display = "none";
-    } else {
-      expansion.style.display = "flex";
+for (let i = 0; i<dropdowns.length;i++) {
+  dropdowns[i].addEventListener("click", function () {
+    this.classList.toggle("expanded");
+// closes nav menu when another is opened
+    for (j=0;j<dropdowns.length;j++) {
+      if (j !== i) {
+        dropdowns[j].nextElementSibling.style.display = "none";
+        dropdowns[j].classList.remove("expanded");
+      }
     }
-  });
-});
+    let expansion = this.nextElementSibling;
+    if (this.classList.contains("expanded")) {
+             expansion.style.display = "flex";
+           } else {
+             expansion.style.display = "none";
+  }
+})}
